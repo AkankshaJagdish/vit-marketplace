2b1391594b8aba38a24b29f0f276ad46ca24ca5a
AkankshaJagdish <akankshaj2021@gmail.com>
2022-07-27 21:43:00 +0530

    Initial commit

---
+++ migrations/1_deploy_contracts.js
@@ -0,0 +1,8 @@
+var VITNFT = artifacts.require("VITNFT");
+var Marketplace = artifacts.require("Marketplace");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Marketplace);
+  const marketplace = await Marketplace.deployed();
+  await deployer.deploy(VITNFT, marketplace.address);
+}
